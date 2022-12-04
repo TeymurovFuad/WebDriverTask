282a4f03c85350653c4cff4489b78dc730ca574f
Fuad Teymurov <Fuad_Teymurov@epam.com>
2022-12-04 14:46:10 +0100

    CReated base class for driver

---
+++ WebDriverTask/DriverManager.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriverTask
+{
+    internal class DriverManager
+    {
+        private static IWebDriver? driver;
+
+        public static IWebDriver Instance()
+        {
+            if (driver == null)
+            {
+                driver = new ChromeDriver();
+                driver.Manage().Timeouts().ImplicitWait.Add(TimeSpan.FromSeconds(5));
+                driver.Manage().Window.Maximize();
+            }
+            return driver;
+        }
+
+        public static void QuitDriver()
+        {
+            driver!.Quit();
+            driver = null;
+        }
+        public static void CloseDriver()
+        {
+            if (driver != null)
+                driver!.Close();
+        }
+
+        public static void ClearAllCookies()
+        {
+            if (driver != null)
+                driver!.Manage().Cookies.DeleteAllCookies();
+        }
+
+        public static void WaitPageToLoad(int secondsToWait = 5)
+        {
+            if (secondsToWait > 0 && driver != null)
+                driver!.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(secondsToWait);
+        }
+    }
+}
