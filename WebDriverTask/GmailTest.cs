af8ec8b3420102938ff5a58c67f0d297700f0bad
Fuad Teymurov <Fuad_Teymurov@epam.com>
2022-12-04 14:45:38 +0100

    Stop working with gmail registration due to blocker related phone number

--- WebDriverTask/GmailTest.cs
+++ WebDriverTask/GmailTest.cs
@@ -5,6 +5,7 @@ using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
 using System.Collections;
 using System.Text.RegularExpressions;
+using NUnit.Framework.Interfaces;
 
 namespace WebDriverTask
 {
@@ -12,31 +13,41 @@ namespace WebDriverTask
     public class GmailTest
     {
         private IWebDriver? _driver;
+        private PageInteractions _interaction;
+        private bool _isFailed;
         private const string _createAccountButtonXPath = "//span[@jsname and text()='Create account']";
         private const string _createPersonalUseAccountButtonXPath = "//span[@jsname and text()='For my personal use']";
         private const string _languageChooserDropdownId = "lang-chooser";
         private const string _languageFromDropDownXPath = "//div[@id='lang-chooser']/div/div/ul/li//span[@jsname]/../..";
+        private const string _errorForExistingUsernameXPath = "//div[text()='You can use letters, numbers & periods']/../div[@aria-live='assertive' and @aria-atomic='true' ]/div[text()='That username is taken. Try another.']";
+        private const string _nextButtonXPath = "//button//span[text()='Next']";
+        private const string _phoneNumberVerificationPageHeaderXPath = "//h1/span[text()='Verifying your phone number']";
 
         [OneTimeSetUp]
-        public void InitializeDriver()
+        public void InitializeTestClass()
         {
             ChromeOptions chromeOptions = new ChromeOptions();
             chromeOptions.AddArgument("--incognito");
             _driver = new ChromeDriver();
             _driver!.Manage().Window.Maximize();
+            _interaction = new PageInteractions();
         }
 
         [SetUp]
         public void TestSetup()
         {
             //Thread.Sleep(2000);
+            if (_isFailed)
+            {
+                Assert.Inconclusive("Previos test failed. Given that all tests are chained, so failure of one may result in failure of all, thereby flow stopped.");
+            }
         }
 
         [Test, Order(1)]
         public void AA_OpenBrowser()
         {
             _driver!.Navigate().GoToUrl("https://accounts.google.com/");
-            WaitPageLoad();
+            _interaction.WaitPageLoad();
             Assert.IsTrue(_driver.Title.StartsWith("Sign in"));
         }
 
@@ -54,67 +65,51 @@ namespace WebDriverTask
         [Test, Order(3)]
         public void BB_VerifyCreateAccountButtonIsVisible()
         {
-            Assert.IsTrue(isElementDisplayed(By.XPath(_createAccountButtonXPath)));
+            Assert.IsTrue(_interaction.isElementDisplayed(By.XPath(_createAccountButtonXPath)));
         }
 
         [Test, Order(4)]
         public void CC_ClickCreateAccountButtonAndVerifyThatNewAccountCreationOptionsAreDisplayed()
         {
-            ClickElement(By.XPath(_createAccountButtonXPath));
-            Assert.True(isElementDisplayed(By.XPath("//ul[@aria-label='Create account']")));
+            _interaction.ClickElement(By.XPath(_createAccountButtonXPath));
+            Assert.True(_interaction.isElementDisplayed(By.XPath("//ul[@aria-label='Create account']")));
         }
 
         [Test, Order(5)]
         public void DD_ClickOptionToCreateAccountForPersonalUseAndVerifyThatPageChanged()
         {
             string initialUrl = _driver!.Url.ToString();
-            ClickElement(By.XPath(_createPersonalUseAccountButtonXPath+"/.."));
-            WaitPageLoad();
+            _interaction.ClickElement(By.XPath(_createPersonalUseAccountButtonXPath+"/.."));
+            _interaction.WaitPageLoad();
             Assert.That(initialUrl != _driver!.Url.ToString());
+            Assert.That(_driver!.FindElement(By.Id("headingText")).Text, Is.EqualTo("Create your Google Account"));
         }
 
-
-
-        public void ClickElement(By locator, bool condition=true)
+        [Test, Order(6)]
+        [TestCase("atmuserName999", "atmsurname888", "Aa1234567__")]
+        public void EE_FillUserDetailsForRegistration(string name, string surname, string password)
         {
-            if (condition)
-            {
-                _driver!.FindElement(locator).Click();
-            }
+            _interaction.SendValuesToElement(By.Id("firstName"), name);
+            _interaction.SendValuesToElement(By.Id("lastName"), surname);
+            _interaction.SendValuesToElement(By.Id("username"), $"{name}.{surname}");
+            Assert.That(_interaction.isElementDisplayed(By.XPath(_errorForExistingUsernameXPath)), Is.False);
+            _interaction.SendValuesToElement(By.Name("Password"), password);
+            _interaction.SendValuesToElement(By.Name("ConfirmPasswd"), password);
+            _interaction.ClickElement(By.XPath(_nextButtonXPath));
+            Assert.IsTrue(_interaction.isElementDisplayed(By.XPath(_phoneNumberVerificationPageHeaderXPath)));
         }
 
-        public void SendValuesToElement(By element, string value)
-        {
-            _driver!.FindElement(element).SendKeys(value);
-        }
 
-        public void WaitPageLoad(int seconds=5)
-        {
-            _driver!.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(seconds);
-        }
-
-        public bool isElementDisplayed(By locator)
-        {
-            try
-            {
-                _driver!.FindElement(locator);
-                return true;
-            }
-            catch (NoSuchElementException)
-            {
-                return false;
-            }
-        }
-
+        #region Methods to interact with elements
         public string GetPageLanguage()
         {
             ICollection<IWebElement> languages;
             string language = string.Empty;
-            if (!isElementDisplayed(By.Id(_languageChooserDropdownId)))
+            if (!_interaction.isElementDisplayed(By.Id(_languageChooserDropdownId)))
             {
                 throw new ElementNotVisibleException();
             }
-            ClickElement(By.Id(_languageChooserDropdownId), _driver!.FindElement(By.Id(_languageChooserDropdownId)).FindElement(By.XPath("./div/div")).GetAttribute("aria-expanded") == "false");
+            _interaction.ClickElement(By.Id(_languageChooserDropdownId), _driver!.FindElement(By.Id(_languageChooserDropdownId)).FindElement(By.XPath("./div/div")).GetAttribute("aria-expanded") == "false");
             languages = _driver!.FindElements(By.XPath(_languageFromDropDownXPath));
             foreach (IWebElement lan in languages)
             {
@@ -124,33 +119,43 @@ namespace WebDriverTask
                     break;
                 }
             }
-            ClickElement(By.Id(_languageChooserDropdownId), _driver!.FindElement(By.Id(_languageChooserDropdownId)).FindElement(By.XPath("./div/div")).GetAttribute("aria-expanded") == "true");
+            _interaction.ClickElement(By.Id(_languageChooserDropdownId), _driver!.FindElement(By.Id(_languageChooserDropdownId)).FindElement(By.XPath("./div/div")).GetAttribute("aria-expanded") == "true");
             return language;
         }
 
         public void ChangePageLanguage(string language)
         {
             ICollection<IWebElement> languages;
-            if (!isElementDisplayed(By.Id(_languageChooserDropdownId)))
+            if (!_interaction.isElementDisplayed(By.Id(_languageChooserDropdownId)))
             {
                 throw new ElementNotVisibleException();
             }
             languages = _driver!.FindElements(By.XPath(_languageFromDropDownXPath));
-            ClickElement(By.Id(_languageChooserDropdownId), _driver!.FindElement(By.Id(_languageChooserDropdownId)).FindElement(By.XPath("./div/div")).GetAttribute("aria-expanded") == "false");
+            _interaction.ClickElement(By.Id(_languageChooserDropdownId), _driver!.FindElement(By.Id(_languageChooserDropdownId)).FindElement(By.XPath("./div/div")).GetAttribute("aria-expanded") == "false");
             foreach (IWebElement lan in languages)
             {
                 if (lan.Text.ToLower().Contains(language))
                 {
                     lan.Click();
-                    WaitPageLoad();
+                    _interaction.WaitPageLoad();
                     break;
                 }
             }
-            ClickElement(By.Id(_languageChooserDropdownId), _driver!.FindElement(By.Id(_languageChooserDropdownId)).FindElement(By.XPath("./div/div")).GetAttribute("aria-expanded") == "true");
+            _interaction.ClickElement(By.Id(_languageChooserDropdownId), _driver!.FindElement(By.Id(_languageChooserDropdownId)).FindElement(By.XPath("./div/div")).GetAttribute("aria-expanded") == "true");
+        }
+        #endregion
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+            {
+                _isFailed = true;
+            }
         }
 
         [OneTimeTearDown]
-        public void DisposeSetup()
+        public void ClassClean()
         {
                 _driver!.Close();
                 _driver.Quit();
