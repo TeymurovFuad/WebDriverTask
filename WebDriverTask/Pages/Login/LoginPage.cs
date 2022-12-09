322dac56983d3e617bb74c9a56c96f1f19643244
Fuad Teymurov <Fuad_Teymurov@epam.com>
2022-12-09 14:50:00 +0100

    Rename direcotry

---
+++ WebDriverTask/Pages/Login/LoginPage.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+using WebDriverTask.Core.BrowserConfigs;
+
+namespace WebDriverTask.Pages.Login
+{
+    public class LoginPage: BasePage
+    {
+        IWebDriver _driver;
+        public LoginPage() : base(BrowserType.Chrome) { }
+
+        public void xxx()
+        {
+        }
+    }
+}
