cdd3474dcd05587ce48129e3c44b3086390e899c
Fuad Teymurov <Fuad_Teymurov@epam.com>
2022-12-09 14:50:28 +0100

    replaced baseintercations with basepage

--- WebDriverTask/Pages/BasePage.cs
+++ WebDriverTask/Pages/BasePage.cs
@@ -1,19 +1,92 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
-using WebDriverTask.Core;
+using WebDriverTask.Core.BrowserConfigs;
+using WebDriverTask.Core.WebDriverConfigs;
 
 namespace WebDriverTask.Pages
 {
-    public abstract class BasePage: BaseInteractions
+    public class BasePage : WebDriverManager
     {
-        IWebDriver driver;
-        public BasePage(Browser browser)
+        private IWebDriver driver { get; set; }
+
+        public BasePage(BrowserType browser)
         {
-            driver = new DriverFactory().CreateDriver(browser, arguments);
+            driver = Instance(browser);
+        }
+
+        public void ClickElement(By locator, bool condition = true)
+        {
+            if (condition && isElementDisplayed(locator))
+            {
+                bool success;
+                int retry = 5;
+                do
+                {
+                    try
+                    {
+                        WaintUntilElementDisplayed(locator);
+                        driver.FindElement(locator).Click();
+                        success = true;
+                    }
+                    catch (StaleElementReferenceException e)
+                    {
+                        success = false;
+                    }
+                    retry--;
+                } while (!success && retry > 0);
+            }
+        }
+
+        public void SendValuesToElement(By locator, string value)
+        {
+            if (isElementDisplayed(locator))
+                driver!.FindElement(locator).SendKeys(value);
+        }
+
+        public void WaitPageLoad(int seconds = 5)
+        {
+            driver!.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(seconds);
+        }
+
+        public bool isElementDisplayed(By locator)
+        {
+            try
+            {
+                driver!.FindElement(locator);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        public void HandleAlert(bool accept = true)
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                if (accept)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                driver.SwitchTo().DefaultContent();
+            }
+            catch (NoAlertPresentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
