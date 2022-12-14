5ed183ad1384d5b233fd35285769389081445175
Fuad Teymurov <Fuad_Teymurov@epam.com>
2022-12-14 21:55:18 +0100

    Implement logout page

---
+++ WebDriverTask/Pages/Gmail/Logout/AccoutDialog.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using WebDriverTask.Core.WebDriverConfigs;
+
+namespace WebDriverTask.Pages.Gmail.Logout
+{
+    public class AccoutDialog: MainPage
+    {
+        public AccoutDialog() : base() { }
+
+        public void OpenAccountDialog(string email)
+        {
+            IWebElement openAccountDialogButton = AccountDialogElements.OpenAccountDialogButton(email);
+            if (isElementDisplayed(openAccountDialogButton))
+            {
+                openAccountDialogButton.Click();
+            }
+        }
+
+        public AccoutDialog And()
+        {
+            return this;
+        }
+
+        public static void SwitchToAccountFrame()
+        {
+            Driver.SwitchToFrame(AccountDialogElements.AccountIFrame);
+        }
+
+        public void ClickSignoutButton()
+        {
+            IWebElement singoutButton = AccountDialogElements.SingOutButton;
+            if (WaitUntilElementIsInteractable(singoutButton))
+            {
+                singoutButton.Click();
+            }
+            else
+            {
+                throw new Exception("Error on click signout button");
+            }
+
+        }
+    }
+}
