64f1330d8e39bb985b658198c224cfa5a89498a4
Fuad Teymurov <Fuad_Teymurov@epam.com>
2022-12-14 21:55:46 +0100

    Add helper from string formatting

--- WebDriverTask/Core/Helpers/StringHelper.cs
+++ WebDriverTask/Core/Helpers/StringHelper.cs
@@ -1,4 +1,6 @@
-﻿namespace WebDriverTask.Core.Helpers
+﻿using System.Text.RegularExpressions;
+
+namespace WebDriverTask.Core.Helpers
 {
     public static class StringHelper
     {public static string GenerateUUID()
@@ -11,6 +13,20 @@
         {
             long epochTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
             return epochTime.ToString();
+        }
+
+        /// <summary>
+        /// Will replace placeholders with a provided values. 
+        /// The number of values should not be less than number of placeholders.
+        /// </summary>
+        public static string? FormatString(string source, params string[] vals)
+        {
+            string pattern = @"{\\d}";
+            Regex regex = new Regex(pattern, RegexOptions.Compiled);
+            MatchCollection matchCollection = regex.Matches(source);
+            if(matchCollection.Count > vals.Length)
+                return null;
+            return string.Format(source, vals);
         }
     }
 }
