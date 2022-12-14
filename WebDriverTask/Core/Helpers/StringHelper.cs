7ede070a4255537bf51d7db5c5f694e24a3999c0
Fuad Teymurov <Fuad_Teymurov@epam.com>
2022-12-14 19:12:43 +0100

    Added helper

---
+++ WebDriverTask/Core/Helpers/StringHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace WebDriverTask.Core.Helpers
+{
+    public static class StringHelper
+    {public static string GenerateUUID()
+        {
+            string uuid = Guid.NewGuid().ToString();
+            return uuid;
+        }
+
+        public static string GetCurrentDateInMilliseconds()
+        {
+            long epochTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            return epochTime.ToString();
+        }
+    }
+}
