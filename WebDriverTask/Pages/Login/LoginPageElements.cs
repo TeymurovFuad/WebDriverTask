00169e54d5541a6de33e0162bfe2940b6c3e6de8
Fuad Teymurov <Fuad_Teymurov@epam.com>
2022-12-14 19:14:37 +0100

    Added more locatoers to the login page

--- WebDriverTask/Pages/Login/LoginPageElements.cs
+++ WebDriverTask/Pages/Login/LoginPageElements.cs
@@ -20,6 +20,37 @@ namespace WebDriverTask.Pages.Login
         [FindsBy(How = How.XPath, Using = $"//*[text()='Not Now']")]
         public IWebElement NotNowButtonOnHelpWorkBetterPage { get; set; }
 
+        [FindsBy(How = How.Id, Using = "lang-chooser")]
+        public IWebElement DropDownToChooseLanguage { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//div[@id='lang-chooser']//ul/li]")]
+        public List<IWebElement> AllLanguagesFromDropDown { get; set; }
+
+        private IWebElement LanguageFromDropDown { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//button[span[contains(text(), 'Next')]]")]
+        public List<IWebElement> NextButton { get; set; }
+
+        public IWebElement FindSpecificLanguageFromDropDown(string language)
+        {
+            foreach (IWebElement element in AllLanguagesFromDropDown)
+            {
+                try
+                {
+                    if (element.FindElement(By.XPath($"//span[contains(text(), {language})]")).Displayed)
+                    {
+                        LanguageFromDropDown = element;
+                        break;
+                    }
+                }
+                catch (NoSuchElementException)
+                {
+                    throw;
+                }
+            }
+            return LanguageFromDropDown;
+        }
+
         public void SkipHelpToWorkBetterPage()
         {
             //Temporary workaround
