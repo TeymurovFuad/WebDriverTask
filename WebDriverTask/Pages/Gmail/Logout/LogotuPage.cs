d40b87a32f03b5dd3f583bce5aab1a4ccf40b477
Fuad Teymurov <Fuad_Teymurov@epam.com>
2022-12-16 00:26:47 +0100

    Added logout page

---
+++ WebDriverTask/Pages/Gmail/Logout/LogotuPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriverTask.Pages.Gmail.Logout
+{
+    public class LogotuPage: MainPage
+    {
+        public static bool isLogoutPageDisplayed()
+        {
+            WaitUntilElementDisplayed(LogoutPageElements.ChooseAnAccout);
+            return isElementDisplayed(LogoutPageElements.ChooseAnAccout);
+        }
+    }
+}
---
+++ WebDriverTask/Pages/Gmail/Logout/LogoutPageElements.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriverTask.Pages.Gmail.Logout
+{
+    public static class LogoutPageElements
+    {
+        [FindsBy(How = How.Id, Using = "//span[text()='Choose an account']")]
+        public static IWebElement ChooseAnAccout { get; private set; }
+    }
+}
