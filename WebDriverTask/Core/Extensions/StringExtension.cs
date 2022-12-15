58c06b20a03c588dae8e32c5566f6ed462e7c197
Fuad Teymurov <Fuad_Teymurov@epam.com>
2022-12-16 00:27:18 +0100

    Added extension to capitilize string

---
+++ WebDriverTask/Core/Extensions/StringExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriverTask.Core.Extensions
+{
+    public static class StringExtension
+    {
+        public static string Capitalise(this string target)
+        {
+            if(string.IsNullOrEmpty(target))
+            {
+                return target;
+            }
+            return char.ToUpper(target[0]) + target.Substring(1);
+        }
+    }
+}
