c6adfbd2f85bc4c9f032d598a669dd4000b0f6b7
Fuad Teymurov <Fuad_Teymurov@epam.com>
2023-01-08 02:06:48 +0100

    extension to work with actions inderectly

---
+++ WebDriverTask/Core/Extensions/ActionsExtensions.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WebDriverTask.Core.Extensions
+{
+    public static class ActionsExtensions
+    {
+        public static Actions CreateActions(this IWebDriver driver)
+        {
+            Actions actions = new Actions(driver);
+            return actions;
+        }
+
+        public static void MoveElementFromTo(this IWebDriver driver, IWebElement sourceElement, IWebElement targetElement)
+        {
+            CreateActions(driver).DragAndDrop(sourceElement, targetElement).Perform();
+        }
+
+        public static Actions SourceElement(this Actions actions, IWebElement sourceElement)
+        {
+            actions.MoveToElement(sourceElement).Perform();
+            return actions;
+        }
+
+        public static Actions MoveToCoordinates(this Actions actions, int x, int y)
+        {
+            actions.MoveByOffset(x, y).Perform();
+            return actions;
+        }
+
+        public static Actions ReleaseElement(this Actions actions)
+        {
+            actions.Release().Perform();
+            return actions;
+        }
+
+        public static Actions ClickAndHoldElement(this Actions actions)
+        {
+            actions.ClickAndHold().Perform();
+            return actions;
+        }
+
+        public static Actions ClickElement(this Actions actions)
+        {
+            actions.Click().Perform();
+            return actions;
+        }
+    }
+}
