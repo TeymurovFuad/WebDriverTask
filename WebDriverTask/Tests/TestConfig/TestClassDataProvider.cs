823f6c2ca14ef69115bb576a9e6083b75d7a059a
Fuad Teymurov <Fuad_Teymurov@epam.com>
2023-01-08 02:09:14 +0100

    Added a class to procided a data to test class on parallel run

---
+++ WebDriverTask/Tests/TestConfig/TestClassDataProvider.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverTask.Core.Browser;
+
+namespace WebDriverTask.Tests.TestConfig
+{
+    public class TestClassDataProvider
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestFixtureData(BrowserType.Chrome);
+                yield return new TestFixtureData(BrowserType.RemoteFirefox);
+                yield return new TestFixtureData(BrowserType.RemoteFirefox);
+            }
+        }
+    }
+}
