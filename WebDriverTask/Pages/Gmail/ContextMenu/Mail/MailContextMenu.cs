be792e5711c78d7a993ef8d01c63934b52933630
Fuad Teymurov <Fuad_Teymurov@epam.com>
2023-01-08 02:08:32 +0100

    Added context menu elements/actions

---
+++ WebDriverTask/Pages/Gmail/ContextMenu/Mail/MailContextMenu.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverTask.Core.Extensions;
+
+namespace WebDriverTask.Pages.Gmail.ContextMenu.Mail
+{
+    public class MailContextMenu : MailContextMenuElements
+    {
+        readonly IWebDriver webDriver;
+        public MailContextMenu(IWebDriver driver) : base(driver) => webDriver = driver;
+
+        public void ClickDelete()
+        {
+            DeleteItem.Click();
+        }
+
+        public void ClickArchive()
+        {
+            ArchiveItem.Click();
+        }
+    }
+}
