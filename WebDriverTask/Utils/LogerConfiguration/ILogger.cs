b1a8f288d00988c722eee9083e2533f832e1d6a3
Fuad Teymurov <Fuad_Teymurov@epam.com>
2023-01-17 00:51:21 +0100

    Added logger

---
+++ WebDriverTask/Utils/LogerConfiguration/ILogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriverTask.Utils.LogerConfiguration
+{
+    public interface ILogger
+    {
+        string CurrentDirectory { get; set; }
+        string LogFileName { get; set; }
+        string LogFilePath { get; set; }
+        void Log(string message);
+    }
+}
---
+++ WebDriverTask/Utils/LogerConfiguration/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+namespace WebDriverTask.Utils.LogerConfiguration
+{
+    public abstract class Logger : ILogger
+    {
+        protected readonly object lockObj = new object();
+
+        public string CurrentDirectory { get; set; }
+        public string LogFileName { get; set; }
+        public string LogFilePath { get; set; }
+
+        public Logger()
+        {
+            lockObj = new object();
+            CurrentDirectory = Environment.CurrentDirectory;
+            LogFileName = "Log.txt";
+            LogFilePath = Path.Combine(CurrentDirectory, "Log", LogFileName);
+        }
+
+        public void Log(string message)
+        {
+            lock (lockObj)
+            {
+                using (StreamWriter streamWriter = new StreamWriter(LogFilePath, true))
+                {
+                    streamWriter.Write("\nLog entry: ");
+                    streamWriter.WriteLine($"{DateTime.Now}");
+                    streamWriter.WriteLine($"   :{message}");
+                    streamWriter.WriteLine("---------------------------------------------");
+                }
+            }
+        }
+    }
+}
