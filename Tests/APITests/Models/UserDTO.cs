1dc64be30ee5cdbceb0562e43b64eac8a57c48e4
Fuad Teymurov <Fuad_Teymurov@epam.com>
2023-01-31 16:21:44 +0100

    Created API test

---
+++ Tests/APITests/Models/UserDTO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Tests.APITests.Models
+{
+    public class UserDTO: IDTO
+    {
+        [JsonPropertyName("id")]
+        public int? Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("username")]
+        public string? Username { get; set; }
+
+        [JsonPropertyName("email")]
+        public string? Email { get; set; }
+
+        [JsonPropertyName("address")]
+        public AddressDTO? Address { get; set; }
+
+        [JsonPropertyName("phone")]
+        public string? Phone { get; set; }
+
+        [JsonPropertyName("website")]
+        public string? Website { get; set; }
+
+        [JsonPropertyName("company")]
+        public CompanyDTO? Company { get; set; }
+    }
+}
