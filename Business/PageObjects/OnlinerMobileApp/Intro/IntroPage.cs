d0ac8a14e4572f3c5f05dad2edd2f84976b87623
Fuad Teymurov <Fuad_Teymurov@epam.com>
2023-02-06 23:38:15 +0100

    Created PO for "onliner" mobile app

---
+++ Business/PageObjects/OnlinerMobileApp/Intro/IntroPage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.UITest;
+
+namespace Business.PageObjects.OnlinerMobileApp.Intro
+{
+    public class IntroPage: IntroPageElements
+    {
+        IApp _app;
+        public IntroPage(IApp app)
+        {
+            _app = app;
+        }
+
+        public void ClickNext()
+        {
+            _app.Tap(nextContainerButtonElement);
+        }
+
+        public void SkipIntro()
+        {
+            int introImages = 5;
+            do
+            {
+                ClickNext();
+                introImages--;
+            }while(introImages > 0);
+        }
+
+        public bool isOpened()
+        {
+            return introTestTitleElement != null;
+        }
+    }
+}
---
+++ Business/PageObjects/OnlinerMobileApp/OnlinerMainPage.cs
@@ -0,0 +1,38 @@
+﻿using Business.PageObjects.Gmail;
+using Business.PageObjects.OnlinerMobileApp.Catalog;
+using Business.PageObjects.OnlinerMobileApp.Intro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace Business.PageObjects.OnlinerMobileApp
+{
+    public class OnlinerMainPage: OnlinerMainPageElements
+    {
+        IApp _app;
+        public IntroPage introPage;
+        public CatalogPage catalogPage;
+
+        public OnlinerMainPage(IApp app)
+        {
+            _app = app;
+            introPage = new IntroPage(app);
+            catalogPage = new CatalogPage(app);
+        }
+
+        public void GoBack()
+        {
+            _app.Tap(navigateUpElement);
+        }
+
+        public string GetHeaderValue()
+        {
+            _app.WaitForElement(headerTextElement);
+            return _app.Query(headerTextElement).First().Text;
+        }
+    }
+}
