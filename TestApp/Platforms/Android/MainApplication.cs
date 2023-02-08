2c1b9f2a23dc92ef49b5607228f4d10b8d2bf04c
Fuad Teymurov <Fuad_Teymurov@epam.com>
2023-02-08 19:15:39 +0100

    feature: added new xamarin project. Tests need to be written

---
+++ TestApp/Platforms/Android/MainApplication.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Runtime;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace TestApp
+{
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp()
+        {
+            AppCenter.Start("",
+                   typeof(Analytics), typeof(Crashes));
+            return MauiProgram.CreateMauiApp();
+        }
+    }
+}
---
+++ Tests/Mobile/CommonMobileHooks.cs
@@ -0,0 +1,42 @@
+﻿using Core.Common.TestConfig;
+using Core.Mobile.Device;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.UITest;
+
+namespace Tests.Mobile
+{
+    public class CommonMobileHooks: CommonHooks
+    {
+        protected IApp app { get; set; }
+        protected DeviceFactory deviceFactory;
+
+        protected CommonMobileHooks()
+        {
+            deviceFactory = new DeviceFactory();
+        }
+
+        [TearDown]
+        public void MakeAScreenshotOnFail()
+        {
+            bool isFailed = TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed;
+            string logDirectory = Directory.GetCurrentDirectory() + "\\Log";
+            if (isFailed)
+            {
+                string className = TestContext.CurrentContext.Test.ClassName;
+                string methodName = TestContext.CurrentContext.Test.MethodName;
+                string arguments = String.Join("_", TestContext.CurrentContext.Test.Arguments);
+                string dateTimeNow = DateTimeOffset.Now.Ticks.ToString();
+                string fileName = $"{className}_{methodName}_{arguments}_{dateTimeNow}";
+                var screenShot = app.Screenshot("");
+                string imageFullPath = Path.Combine(logDirectory, fileName + screenShot.Extension);
+                screenShot.MoveTo(imageFullPath);
+            }
+        }
+    }
+}
